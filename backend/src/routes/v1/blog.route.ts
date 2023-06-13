553a674fad1d62f10bc6ca35f2e254126413f55e
Aayusharma07 <Aayush.Sharma@motherson.com>
2023-06-13 16:28:28 +0530

    api created

--- backend/src/routes/v1/blog.route.ts
+++ backend/src/routes/v1/blog.route.ts
@@ -1,7 +1,8 @@
 import express, { Router } from 'express';
-import { createBlog } from '../../controllers/blog.controller';
+import { createBlog, getAllBlogs } from '../../controllers/blog.controller';
 const router: Router = express.Router();
 
-router.post('/create', createBlog)
+router.post('/create', createBlog);
+router.get('/alldata', getAllBlogs);
 
 export default router;
